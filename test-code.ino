d9a4222bbcf7cade3dc15089ed51592e13859681
Marvin M. Ramos <115031416+Crispkreme@users.noreply.github.com>
2024-04-27 02:58:09 +0800

    Update test-code.ino

--- test-code.ino
+++ test-code.ino
@@ -1,3 +1,9 @@
+// SHOW AND DISPLAY USER DATA
+// SELECT PURPOSE TO LOGIN IN
+// GO TO ROOM WHEN DATA IS CORRECT
+// SAVE THE CURRENT USER WHO ENTER IN THE ROOM
+// GET THE USER ID, GET THE TIME AND DATE
+
 #include "Adafruit_Fingerprint.h" 
 #include <EEPROM.h> 
 #include <SoftwareSerial.h>
@@ -9,7 +15,6 @@ uint8_t id;
 SoftwareSerial fingerPrint(2, 3);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&fingerPrint);
 
-#define match 5
 #define indFinger 7
 #define buzzer 5
 
@@ -19,29 +24,128 @@ int user1, user2, user3, user4, user5, user6, user7, user8, user9, user10;
 
 DateTime now;
 
+// FOR REGISTRATION
 const int button_register_back = A0;
 const int button_forward = A1;
 const int button_reverse = A2;
 const int button_delete_ok = A3;
+
+// FOR ROOMS
+const int button_room_1 = 8;
+const int button_room_2 = 9;
+const int button_room_3 = 10;
+const int button_room_4 = 11;
+
+// GLOBAL VARIABLES
+int buttonRegisterBackState;
+int buttonForwardState;
+int buttonReverseState;
+int buttonDeleteOkState;
+int buttonRoom1;
+int buttonRoom2;
+int buttonRoom3;
+int buttonRoom4;
 
 void setup() {
   Serial.begin(9600);
   finger.begin(57600);
   Serial.begin(9600);
 
+  // PIN CONFIGURATION
   pinMode(button_register_back, INPUT_PULLUP);
   pinMode(button_forward, INPUT_PULLUP);
   pinMode(button_reverse, INPUT_PULLUP);
   pinMode(button_delete_ok, INPUT_PULLUP);
-  
-  pinMode(match, INPUT_PULLUP);
+
+  pinMode(button_room_1, INPUT_PULLUP);
+  pinMode(button_room_2, INPUT_PULLUP);
+  pinMode(button_room_3, INPUT_PULLUP);
+  pinMode(button_room_4, INPUT_PULLUP);
+
   pinMode(buzzer, OUTPUT);
   pinMode(indFinger, OUTPUT);
 
-  int buttonRegisterBackState = digitalRead(button_register_back);
-  int buttonForwardState = digitalRead(button_forward);
-  int buttonReverseState = digitalRead(button_reverse);
-  int buttonDeleteOkState = digitalRead(button_delete_ok);
+  donwloadExistingData();
+
+  resettingExistingData();
+
+  buzzerSound();
+
+  for (int i = 1000; i < 1000 + records; i++) {
+    if (EEPROM.read(i) == 0xff) EEPROM.write(i, 0);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC is NOT running!");
+    rtc.adjust(DateTime(2018, 6, 7, 11, 0, 0));
+  }
+}
+
+void loop() {
+  now = rtc.now();
+  int result = getFingerprintIDez();
+
+  // LOGIN TAKE PLACE
+  buttonRoom1 = digitalRead(button_room_1);
+  buttonRoom2 = digitalRead(button_room_2);
+  buttonRoom3 = digitalRead(button_room_3);
+  buttonRoom4 = digitalRead(button_room_4);
+
+  if (buttonRoom1 == HIGH) {
+    if (result > 0) {
+      digitalWrite(indFinger, LOW);
+      buzzerSound();
+      Serial.println('Room: 1');
+      Serial.println('Date: 4/27/24');
+      Serial.println('user ID: ' + result);
+      attendance(result);
+      digitalWrite(indFinger, HIGH);
+      return;
+    }
+  } else if (buttonRoom2 == HIGH) {
+    if (result > 0) {
+      digitalWrite(indFinger, LOW);
+      buzzerSound();
+      Serial.println('Room: 2');
+      Serial.println('Date: 4/27/24');
+      Serial.println('user ID: ' + result);
+      attendance(result);
+      digitalWrite(indFinger, HIGH);
+      return;
+    }
+  } else if (buttonRoom3 == HIGH) {
+    if (result > 0) {
+      digitalWrite(indFinger, LOW);
+      buzzerSound();
+      Serial.println('Room: 3');
+      Serial.println('Date: 4/27/24');
+      Serial.println('user ID: ' + result);
+      attendance(result);
+      digitalWrite(indFinger, HIGH);
+      return;
+    }
+  } else if (buttonRoom4 == HIGH) {
+    if (result > 0) {
+      digitalWrite(indFinger, LOW);
+      buzzerSound();
+      Serial.println('Room: 4');
+      Serial.println('Date: 4/27/24');
+      Serial.println('user ID: ' + result);
+      attendance(result);
+      digitalWrite(indFinger, HIGH);
+      return;
+    }
+  } else {
+    Serial.println('Error: Error when entering the rooms');
+  }  
+
+  checkKeys();
+  delay(300);
+}
+
+void donwloadExistingData()
+{
+  buttonRegisterBackState = digitalRead(button_register_back);
 
   if (buttonRegisterBackState == HIGH) {
     
@@ -100,6 +204,11 @@ void setup() {
       }
     }
   }
+}
+
+void resettingExistingData() 
+{
+  buttonDeleteOkState = digitalRead(button_delete_ok);
 
   if (buttonDeleteOkState == HIGH) {
 
@@ -108,41 +217,6 @@ void setup() {
     for (int i = 1000; i < 1005; i++) EEPROM.write(i, 0);
     for (int i = 0; i < 841; i++) EEPROM.write(i, 0xff);
   }
-
-  buzzerSound();
-
-  for (int i = 1000; i < 1000 + records; i++) {
-    if (EEPROM.read(i) == 0xff) EEPROM.write(i, 0);
-  }
-
-  if (rtc.lostPower()) {
-    Serial.println("RTC is NOT running!");
-    rtc.adjust(DateTime(2018, 6, 7, 11, 0, 0));
-  }
-}
-
-void loop() {
-  now = rtc.now();
-
-  int buttonRegisterBackState = digitalRead(button_register_back);
-  int buttonForwardState = digitalRead(button_forward);
-  int buttonReverseState = digitalRead(button_reverse);
-  int buttonDeleteOkState = digitalRead(button_delete_ok);
-
-  int result = getFingerprintIDez();
-
-  if (result > 0) {
-    digitalWrite(indFinger, LOW);
-    
-    buzzerSound();
-
-    attendance(result);
-    digitalWrite(indFinger, HIGH);
-    return;
-  }
-
-  checkKeys();
-  delay(300);
 }
 
 void buzzerSound()
@@ -151,12 +225,70 @@ void buzzerSound()
   delay(500);
   digitalWrite(buzzer, LOW);
 }
+
 void displayMessage(String message)
 {
   Serial.println("Please wait");
   Serial.println(message);
   Serial.println();
 }
+
+void checkKeys() {
+
+  buttonRegisterBackState = digitalRead(button_register_back);
+  buttonDeleteOkState = digitalRead(button_delete_ok);
+
+  if (buttonRegisterBackState == HIGH) {
+    actionData(1);
+  } else if (buttonDeleteOkState == HIGH) {
+    actionData(2);
+  }
+}
+
+void actionData(int action) {
+  int count = 1;
+
+  while (1) {
+    if (buttonForwardState == HIGH) {
+      count++;
+      if (count > records) count = 1;
+      delay(500);
+    } else if (buttonReverseState == HIGH) {
+      count--;
+      if (count < 1) count = records;
+      delay(500);
+    } else if (buttonDeleteOkState == HIGH) {
+      id = count;
+
+      if(action == 1) {
+        getFingerprintEnroll();
+
+        for (int i = 0; i < records; i++) {
+          if (EEPROM.read(i) != 0xff) {
+            EEPROM.write(i, id);
+            break;
+          }
+        }
+        return;
+
+      } else {
+        deleteFingerprint(id);
+
+        for (int i = 0; i < records; i++) {
+          if (EEPROM.read(i) == id) {
+            EEPROM.write(i, 0xff);
+            break;
+          }
+        }
+        return;
+      }
+      
+    } else if (buttonRegisterBackState == HIGH) {
+      return;
+    }
+  }
+}
+
 void attendance(int id) {
   int user = 0, eepLoc = 0;
 
@@ -220,68 +352,52 @@ void attendance(int id) {
   EEPROM.write(1002, user3);
   EEPROM.write(1003, user4);
 }
-void checkKeys() {
 
-  int buttonRegisterBackState = digitalRead(button_register_back);
-  int buttonForwardState = digitalRead(button_forward);
-  int buttonReverseState = digitalRead(button_reverse);
-  int buttonDeleteOkState = digitalRead(button_delete_ok);
-
-  if (buttonRegisterBackState == HIGH) {
-    actionData(1);
-  } else if (buttonDeleteOkState == HIGH) {
-    actionData(2);
+void download(int eepIndex) {
+  if (EEPROM.read(eepIndex) != 0xff) {
+    if (EEPROM.read(eepIndex) < 10)
+      Serial.print('0');
+      Serial.print(EEPROM.read(eepIndex++));
+      Serial.print(':');
+    if (EEPROM.read(eepIndex) < 10)
+      Serial.print('0');
+      Serial.print(EEPROM.read(eepIndex++));
+      Serial.print(':');
+    if (EEPROM.read(eepIndex) < 10)
+      Serial.print('0');
+      Serial.print(EEPROM.read(eepIndex++));
+      Serial.print(" D->");
+    if (EEPROM.read(eepIndex) < 10)
+      Serial.print('0');
+      Serial.print(EEPROM.read(eepIndex++));
+      Serial.print('/');
+    if (EEPROM.read(eepIndex) < 10)
+      Serial.print('0');
+      Serial.print(EEPROM.read(eepIndex++));
+      Serial.print('/');
+      Serial.print(EEPROM.read(eepIndex++) << 8 | EEPROM.read(eepIndex++));
+  } else {
+    Serial.print("---------------------------");
   }
+  Serial.print(" ");
 }
-void actionData(int action) {
 
-  int buttonRegisterBackState = digitalRead(button_register_back);
-  int buttonForwardState = digitalRead(button_forward);
-  int buttonReverseState = digitalRead(button_reverse);
-  int buttonDeleteOkState = digitalRead(button_delete_ok);
-  
-  int count = 1;
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
 
-  while (1) {
-    if (buttonForwardState == HIGH) {
-      count++;
-      if (count > records) count = 1;
-      delay(500);
-    } else if (buttonReverseState == HIGH) {
-      count--;
-      if (count < 1) count = records;
-      delay(500);
-    } else if (buttonDeleteOkState == HIGH) {
-      id = count;
+  if (p != FINGERPRINT_OK)
+    return -1;
 
-      if(action == 1) {
-        getFingerprintEnroll();
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)
+    return -1;
 
-        for (int i = 0; i < records; i++) {
-          if (EEPROM.read(i) != 0xff) {
-            EEPROM.write(i, id);
-            break;
-          }
-        }
-        return;
-
-      } else {
-        deleteFingerprint(id);
-
-        for (int i = 0; i < records; i++) {
-          if (EEPROM.read(i) == id) {
-            EEPROM.write(i, 0xff);
-            break;
-          }
-        }
-        return;
-      }
-      
-      
-    } else if (buttonRegisterBackState == HIGH) {
-      return;
-    }
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK) {
+    return -1;
   }
+
+  return finger.fingerID;
 }
 
 uint8_t getFingerprintEnroll() {
@@ -352,23 +468,7 @@ uint8_t getFingerprintEnroll() {
     return p;
   }
 }
-int getFingerprintIDez() {
-  uint8_t p = finger.getImage();
 
-  if (p != FINGERPRINT_OK)
-    return -1;
-
-  p = finger.image2Tz();
-  if (p != FINGERPRINT_OK)
-    return -1;
-
-  p = finger.fingerFastSearch();
-  if (p != FINGERPRINT_OK) {
-    return -1;
-  }
-
-  return finger.fingerID;
-}
 uint8_t deleteFingerprint(uint8_t id) {
   uint8_t p = -1;
 
@@ -377,32 +477,4 @@ uint8_t deleteFingerprint(uint8_t id) {
     return p;
   }
   return p;
-}
-void download(int eepIndex) {
-  if (EEPROM.read(eepIndex) != 0xff) {
-    if (EEPROM.read(eepIndex) < 10)
-      Serial.print('0');
-      Serial.print(EEPROM.read(eepIndex++));
-      Serial.print(':');
-    if (EEPROM.read(eepIndex) < 10)
-      Serial.print('0');
-      Serial.print(EEPROM.read(eepIndex++));
-      Serial.print(':');
-    if (EEPROM.read(eepIndex) < 10)
-      Serial.print('0');
-      Serial.print(EEPROM.read(eepIndex++));
-      Serial.print(" D->");
-    if (EEPROM.read(eepIndex) < 10)
-      Serial.print('0');
-      Serial.print(EEPROM.read(eepIndex++));
-      Serial.print('/');
-    if (EEPROM.read(eepIndex) < 10)
-      Serial.print('0');
-      Serial.print(EEPROM.read(eepIndex++));
-      Serial.print('/');
-      Serial.print(EEPROM.read(eepIndex++) << 8 | EEPROM.read(eepIndex++));
-  } else {
-    Serial.print("---------------------------");
-  }
-  Serial.print(" ");
 }
