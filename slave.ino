69f05b176bb44bb05d40c847f9b619ad1512d6da
Marvin M. Ramos <115031416+Crispkreme@users.noreply.github.com>
2024-05-04 14:16:00 +0800

    Update slave.ino

--- slave.ino
+++ slave.ino
@@ -1,1 +1,17 @@
+char mystr1[10];
+char mystr2[10];
 
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  Serial.readBytes(mystr1, 1);
+  Serial.println(mystr1); 
+
+  Serial.readBytes(mystr2, 6);
+  Serial.println(mystr2); 
+
+  delay(1000);
+}
