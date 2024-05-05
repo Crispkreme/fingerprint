ee7b043c2cdad77fc5e4f0f8bfb123953c163e08
Marvin M. Ramos <115031416+Crispkreme@users.noreply.github.com>
2024-05-05 15:55:26 +0800

    Update slave.ino

--- slave.ino
+++ slave.ino
@@ -1,10 +1,12 @@
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include "HD44780_LCD_PCF8574.h"
 
 File myFile;
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
+HD44780LCD myLCD(4, 16, 0x27, &Wire);
+
+#define DISPLAY_DELAY_INIT 50
 
 void writeToFile(String message) {
   Serial.print("Initializing SD card...");
@@ -31,14 +33,21 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Hello World");
-
+  delay(DISPLAY_DELAY_INIT);
+  myLCD.PCF8574_LCDInit(myLCD.LCDCursorTypeOn);
+  myLCD.PCF8574_LCDClearScreen();
+  myLCD.PCF8574_LCDBackLightSet(true);
+  myLCD.PCF8574_LCDGOTO(myLCD.LCDLineNumberOne, 0);
 }
 
 void loop() {
+
+  String titleString = "SPELVA";
+
+  myLCD.PCF8574_LCDGOTO(myLCD.LCDLineNumberOne, 0);
+  myLCD.PCF8574_LCDSendString(titleString.c_str());
+  myLCD.PCF8574_LCDSendChar(' ');
+  
   if (Serial.available()) {
 
     String message = Serial.readStringUntil('\n');
